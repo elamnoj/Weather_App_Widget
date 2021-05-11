d82de5f4aa8b346c747791e39398fa74928208e4
elamnoj <male.jonathan@gmail.com>
2021-05-11 15:50:09 -0500

    updated js fadeIn/Out effect

--- app.js
+++ app.js
@@ -5,29 +5,23 @@ var contact = document.getElementById('form-handler')
 var button = document.getElementById('button2')
 
 $('#form-handler').on('submit', function (e) {
-    contact.remove()
+    $('#form-handler').fadeOut('slow');
     e.preventDefault();
-    console.log($('#list-of-options').val())
     $.ajax({
         url: `https://api.openweathermap.org/data/2.5/forecast?q=${city_input.val()}&units=${options.val()}&appid=fff7b5e9db3bb9a75d36e383de9e4ee7`,
         method: "GET",
         success: function(data){
             console.log('success',data);
             var main = data.list[0].main;
-            city = data.city.name
-            temp = Math.round(main.temp);
-            hi = Math.round(main.temp_max);
-            low = Math.round(main.temp_min);
-            humid = main.humidity
-            console.log(city)
-            console.log(temp) 
+            $('#weather').hide() 
             render(main, data)
+            $('#weather').fadeIn(3000)
         }
     });
 });
 
-
 function render(main, data){
+    
     $('#city').html(`<h1>${data.city.name}</h1>`)
     $('#temp').html(`<h4>Current Temp: ${Math.round(main.temp)}&#176</h4>`)
     $('#humid').html(`<h5>Humidity: ${main.humidity}%</h5>`)
@@ -36,6 +30,8 @@ function render(main, data){
     button.style.display = 'block';
 
 }
+
+
 
 // $('#button2').on('submit', function (f) {
 //     render.remove();
